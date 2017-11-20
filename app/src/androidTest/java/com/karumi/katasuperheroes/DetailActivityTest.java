b17204954feb65323c00b4355c7cefd61bc9f39f
German Franco <german.franco@vass.es>
2017-11-20 18:34:35 +0100

    Detail TEsts

---
+++ app/src/androidTest/java/com/karumi/katasuperheroes/DetailActivityTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 Karumi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.karumi.katasuperheroes;
+
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import com.karumi.katasuperheroes.di.MainComponent;
+import com.karumi.katasuperheroes.di.MainModule;
+import com.karumi.katasuperheroes.matchers.ToolbarMatcher;
+import com.karumi.katasuperheroes.model.SuperHero;
+import com.karumi.katasuperheroes.model.SuperHeroesRepository;
+import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
+import com.karumi.katasuperheroes.ui.view.MainActivity;
+import com.karumi.katasuperheroes.ui.view.SuperHeroDetailActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import it.cosenonjaviste.daggermock.DaggerMockRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class DetailActivityTest {
+
+    @Rule
+    public DaggerMockRule<MainComponent> daggerRule =
+            new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
+                    new DaggerMockRule.ComponentSetter<MainComponent>() {
+                        @Override
+                        public void setComponent(MainComponent component) {
+                            SuperHeroesApplication app =
+                                    (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation()
+                                            .getTargetContext()
+                                            .getApplicationContext();
+                            app.setComponent(component);
+                        }
+                    });
+
+    @Rule
+    public IntentsTestRule<SuperHeroDetailActivity> activityRule =
+            new IntentsTestRule<>(SuperHeroDetailActivity.class, true, false);
+
+    @Mock
+    SuperHeroesRepository repository;
+
+   @Test
+   public void shouldBeShownCorrectName(){
+       SuperHero superHero = givenSomeSuperHero();
+
+       startActivity();
+
+       onView(withId(R.id.tv_super_hero_name)).check(matches(withText(superHero.getName())));
+   }
+    private SuperHero givenSomeSuperHero() {
+       return givenSomeSuperHero(false);
+    }
+
+    private SuperHero givenSomeSuperHero(boolean isAvenger) {
+       SuperHero superHero = new SuperHero("Pagafantas", null, isAvenger, "Cualquier cosa");
+
+       when(repository.getByName(anyString())).thenReturn(superHero);
+
+       return superHero;
+    }
+
+    private SuperHeroDetailActivity startActivity() {
+
+        Intent i = new Intent();
+        i.putExtra("super_hero_name_key", "ANY_NAME");
+
+       return activityRule.launchActivity(i);
+    }
+
+    @Test
+    public void shouldBeShownAvengerBadge(){
+        givenSomeSuperHero(true);
+
+        startActivity();
+
+        onView(withId(R.id.iv_avengers_badge)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void shouldShowNameInToolbar(){
+        givenSomeSuperHero();
+
+        startActivity();
+
+        ToolbarMatcher.onToolbarWithTitle("ANY_NAME");
+    }
+}
